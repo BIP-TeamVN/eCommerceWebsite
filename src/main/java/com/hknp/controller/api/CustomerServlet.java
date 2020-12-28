8e974a2cf31eab886d8e3d5c306a687c0ca409b9
QuocNam <namtranquoc322@gmail.com>
2020-12-28 08:50:23 +0700

    fix customerServlet

--- src/main/java/com/hknp/controller/api/CustomerServlet.java
+++ src/main/java/com/hknp/controller/api/CustomerServlet.java
@@ -1,7 +1,14 @@
 package com.hknp.controller.api;
 
+import com.hknp.model.cons.UserCons;
 import com.hknp.model.dao.CustomerDAO;
+import com.hknp.model.dao.UserDAO;
 import com.hknp.model.entity.CustomerEntity;
+import com.hknp.model.entity.UserEntity;
+import com.hknp.utils.Base64Utils;
+import com.hknp.utils.DateTimeUtils;
+import com.hknp.utils.HashUtils;
+import com.hknp.utils.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
