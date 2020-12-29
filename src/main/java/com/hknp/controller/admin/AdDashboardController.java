5c720f4d3c5f3502fc5e7042664a2a837c323a2c
QuocNam <namtranquoc322@gmail.com>
2020-12-29 08:56:03 +0700

    add filter

--- src/main/java/com/hknp/controller/admin/AdDashboardController.java
+++ src/main/java/com/hknp/controller/admin/AdDashboardController.java
@@ -13,22 +13,10 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/admin"})
-public class AdminDashboardController extends HttpServlet {
+public class AdDashboardController extends HttpServlet {
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      HttpSession session = req.getSession();
-      try {
-         Long id = (Long) session.getAttribute("id");
-         String userType = UserDAO.getInstance().getById(id).getUserType();
-         if (userType.equals(Cons.User.USER_TYPE_ADMIN)){
-            ServletUtils.forward(req, resp, "/view/admin/ad-dashboard.jsp");
-         }
-         else {
-            ServletUtils.forward(req, resp, "/login");
-         }
-      } catch (Exception e) {
-         ServletUtils.forward(req, resp, "/login");
-      }
+      ServletUtils.forward(req, resp, "/view/admin/ad-dashboard.jsp");
    }
 
    @Override
