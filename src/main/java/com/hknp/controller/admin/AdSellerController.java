cfe478d459ef316abe146d3f949388a35df0f5cb
DESKTOP-VJSMPL6\Phuc_Tran <nguyentranphuc.0310@gmail.com>
2020-12-31 08:32:36 +0700

    Update adsellercontroller

--- src/main/java/com/hknp/controller/admin/AdSellerController.java
+++ src/main/java/com/hknp/controller/admin/AdSellerController.java
@@ -1,6 +1,9 @@
 package com.hknp.controller.admin;
 
+import com.hknp.model.dao.EmployeeDAO;
+import com.hknp.model.dao.SellerDAO;
 import com.hknp.utils.ServletUtils;
+import com.hknp.utils.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +16,21 @@ import java.io.IOException;
 public class AdSellerController extends HttpServlet {
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      Long totalRows = SellerDAO.getInstance().count();
+      String page = req.getParameter("page");
+
+      Long currentPage = StringUtils.toLong(page);
+      Long totalPage = (totalRows / 10) + ((totalRows % 10 == 0) ? 0 : 1);
+
+      if (currentPage > totalPage) {
+         currentPage = totalPage;
+      }
+      if (currentPage < 1) {
+         currentPage = 1L;
+      }
+
+      req.setAttribute("totalPage", totalPage);
+      req.setAttribute("currentPage", currentPage);
       ServletUtils.forward(req, resp, "/view/admin/ad-seller.jsp");
    }
 
