c3a76fe4db70ca82433d71c2cdf61fbc15786ac3
hohuyhoangg <hoangho1147@gmail.com>
2021-01-12 11:01:34 +0700

    update check authentication api

--- src/main/java/com/hknp/controller/api/AddressServlet.java
+++ src/main/java/com/hknp/controller/api/AddressServlet.java
@@ -3,6 +3,7 @@ package com.hknp.controller.api;
 import com.hknp.model.dao.*;
 import com.hknp.model.entity.AddressEntity;
 import com.hknp.model.entity.Cons;
+import com.hknp.utils.ServletUtils;
 import com.hknp.utils.StringUtils;
 
 import javax.servlet.ServletException;
@@ -19,36 +20,18 @@ import java.util.List;
 @WebServlet(urlPatterns = {"/api/address"})
 public class AddressServlet extends HttpServlet {
 
-   public boolean isAuthentication(HttpServletRequest req) {
-      HttpSession session = req.getSession(false);
-      Long id = (Long) session.getAttribute("id");
-
-      if (id != null) {
-         String userType = UserDAO.getInstance().getUserType(id);
-         return userType.equals(Cons.User.USER_TYPE_ADMIN) || userType.equals(Cons.User.USER_TYPE_EMPLOYEE);
-      }
-
-      return false;
-   }
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
       resp.setContentType("text/html; charset=UTF-8");
-      String result = "";
 
-      if (isAuthentication(req)) {
-         ArrayList<AddressEntity> listAddress = AddressDAO.getInstance().gets();
-         List<String> listJsonStr = new ArrayList<>();
+      ArrayList<AddressEntity> listAddress = AddressDAO.getInstance().gets();
+      List<String> listJsonStr = new ArrayList<>();
 
-         for (AddressEntity address : listAddress) {
-            listJsonStr.add(address.toJson());
-         }
-         result = "[" + String.join(", ", listJsonStr) + "]";
-      } else {
-         result = "Access denied";
+      for (AddressEntity address : listAddress) {
+         listJsonStr.add(address.toJson());
       }
-      try (PrintWriter out = resp.getWriter()) {
-         out.write(result);
-      }
+
+      ServletUtils.printWrite(resp, "[" + String.join(", ", listJsonStr) + "]");
 
    }
 
@@ -56,39 +39,35 @@ public class AddressServlet extends HttpServlet {
    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
       resp.setContentType("text/html; charset=UTF-8");
       String result = "";
-      if (isAuthentication(req)) {
-         try {
-            String addressStreet = req.getParameter("address-street");
-            String provinceId = req.getParameter("province");
-            String districtId = req.getParameter("district");
-            String communeId = req.getParameter("commune");
-            String userId = req.getParameter("user-id");
-            String fullName = req.getParameter("full-name");
-            String addressName = req.getParameter("address-name");
-            String phoneNumber = req.getParameter("phone-number");
+      try {
+         String addressStreet = req.getParameter("address-street");
+         String provinceId = req.getParameter("province");
+         String districtId = req.getParameter("district");
+         String communeId = req.getParameter("commune");
+         String userId = req.getParameter("user-id");
+         String fullName = req.getParameter("full-name");
+         String addressName = req.getParameter("address-name");
+         String phoneNumber = req.getParameter("phone-number");
 
-            AddressEntity newAddress = new AddressEntity();
+         AddressEntity newAddress = new AddressEntity();
 
-            newAddress.setStreet(addressStreet);
-            newAddress.setProvinceEntity(ProvinceDAO.getInstance().getById(provinceId));
-            newAddress.setDistrictEntity(DistrictDAO.getInstance().getById(districtId));
-            newAddress.setCommuneEntity(CommuneDAO.getInstance().getById(communeId));
-            newAddress.setUserId(StringUtils.toLong(userId));
-            newAddress.setFullName(fullName);
-            newAddress.setAddressName(addressName);
-            newAddress.setPhoneNumber(phoneNumber);
+         newAddress.setStreet(addressStreet);
+         newAddress.setProvinceEntity(ProvinceDAO.getInstance().getById(provinceId));
+         newAddress.setDistrictEntity(DistrictDAO.getInstance().getById(districtId));
+         newAddress.setCommuneEntity(CommuneDAO.getInstance().getById(communeId));
+         newAddress.setUserId(StringUtils.toLong(userId));
+         newAddress.setFullName(fullName);
+         newAddress.setAddressName(addressName);
+         newAddress.setPhoneNumber(phoneNumber);
 
-            Long insertResult = AddressDAO.getInstance().insert(newAddress);
-            if (insertResult != 0) {
-               result += "true\n" + insertResult;
-            } else {
-               result += "false\nError while insert address\n";
-            }
-         } catch (Exception e) {
-            result += "false\n" + e.getMessage();
+         Long insertResult = AddressDAO.getInstance().insert(newAddress);
+         if (insertResult != 0) {
+            result += "true\n" + insertResult;
+         } else {
+            result += "false\nError while insert address\n";
          }
-      }else {
-         result = "Access denied";
+      } catch (Exception e) {
+         result += "false\n" + e.getMessage();
       }
       try (PrintWriter out = resp.getWriter()) {
          out.write(result);
@@ -99,40 +78,37 @@ public class AddressServlet extends HttpServlet {
    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
       resp.setContentType("text/html; charset=UTF-8");
       String result = "";
-      if (isAuthentication(req)) {
-         try {
-            String addressId = req.getParameter("id");
-            String addressStreet = req.getParameter("address-street");
-            String provinceId = req.getParameter("province");
-            String districtId = req.getParameter("district");
-            String communeId = req.getParameter("commune");
-            String fullName = req.getParameter("full-name");
-            String addressName = req.getParameter("address-name");
-            String phoneNumber = req.getParameter("phone-number");
+      try {
+         String addressId = req.getParameter("id");
+         String addressStreet = req.getParameter("address-street");
+         String provinceId = req.getParameter("province");
+         String districtId = req.getParameter("district");
+         String communeId = req.getParameter("commune");
+         String fullName = req.getParameter("full-name");
+         String addressName = req.getParameter("address-name");
+         String phoneNumber = req.getParameter("phone-number");
 
-            AddressEntity updateAddress = AddressDAO.getInstance().getById(StringUtils.toLong(addressId));
+         AddressEntity updateAddress = AddressDAO.getInstance().getById(StringUtils.toLong(addressId));
 
-            updateAddress.setStreet(addressStreet);
-            updateAddress.setProvinceEntity(ProvinceDAO.getInstance().getById(provinceId));
-            updateAddress.setDistrictEntity(DistrictDAO.getInstance().getById(districtId));
-            updateAddress.setCommuneEntity(CommuneDAO.getInstance().getById(communeId));
-            updateAddress.setFullName(fullName);
-            updateAddress.setAddressName(addressName);
-            updateAddress.setPhoneNumber(phoneNumber);
+         updateAddress.setStreet(addressStreet);
+         updateAddress.setProvinceEntity(ProvinceDAO.getInstance().getById(provinceId));
+         updateAddress.setDistrictEntity(DistrictDAO.getInstance().getById(districtId));
+         updateAddress.setCommuneEntity(CommuneDAO.getInstance().getById(communeId));
+         updateAddress.setFullName(fullName);
+         updateAddress.setAddressName(addressName);
+         updateAddress.setPhoneNumber(phoneNumber);
 
-            Boolean updateResult = AddressDAO.getInstance().update(updateAddress);
-            if (updateResult == false)
-               result += "false\nError while update address\n";
-            else {
-               result += "True";
-            }
-
-         } catch (Exception e) {
-            result += "false\n" + e.getMessage();
+         Boolean updateResult = AddressDAO.getInstance().update(updateAddress);
+         if (updateResult == false)
+            result += "false\nError while update address\n";
+         else {
+            result += "True";
          }
-      } else {
-         result = "Access denied";
+
+      } catch (Exception e) {
+         result += "false\n" + e.getMessage();
       }
+
       try (PrintWriter out = resp.getWriter()) {
          out.write(result);
       }
--- src/main/java/com/hknp/controller/api/SellerServlet.java
+++ src/main/java/com/hknp/controller/api/SellerServlet.java
@@ -26,7 +26,7 @@ public class SellerServlet extends HttpServlet {
 
       if (id != null) {
          String userType = UserDAO.getInstance().getUserType(id);
-         return userType.equals(Cons.User.USER_TYPE_ADMIN) || userType.equals(Cons.User.USER_TYPE_SELLER) || userType.equals(Cons.User.USER_TYPE_EMPLOYEE);
+         return  userType.equals(Cons.User.USER_TYPE_SELLER);
       }
       return false;
    }
@@ -42,30 +42,54 @@ public class SellerServlet extends HttpServlet {
       return false;
    }
    @Override
+   protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      if (isAuthentication(req)) {
+         switch (req.getMethod()) {
+            case "GET":
+               doGet(req, resp);
+               break;
+            default:
+               ServletUtils.printWrite(resp, "Method not found");
+         }
+      }
+      if (isAuthenticationAdmin(req)) {
+         switch (req.getMethod()) {
+            case "GET":
+               doGet(req, resp);
+               break;
+            case "POST":
+               doPost(req, resp);
+               break;
+            case "PUT":
+            case "PATCH":
+               doPut(req, resp);
+               break;
+            case "DELETE":
+               doDelete(req, resp);
+               break;
+            default:
+               ServletUtils.printWrite(resp, "Method not found");
+         }
+      } else {
+         ServletUtils.printWrite(resp, "Access denied");
+      }
+   }
+   @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
       resp.setContentType("text/html; charset=UTF-8");
-      String result = "";
 
-      if (isAuthentication(req)) {
-         String pagePara = req.getParameter("page");
-         Integer page = StringUtils.toInt(pagePara);
-         if (page <= 0) {
-            page = 1;
-         }
-
-         ArrayList<SellerEntity> listSeller = SellerDAO.getInstance().gets((page - 1) * 10, 10);
-         List<String> listJsonStr = new ArrayList<>();
-
-         for (SellerEntity seller : listSeller) {
-            listJsonStr.add(seller.toJson());
-         }
-         result = "[" + String.join(", ", listJsonStr) + "]";
-      } else {
-         result = "Access denied";
+      String pagePara = req.getParameter("page");
+      Integer page = StringUtils.toInt(pagePara);
+      if (page <= 0) {
+         page = 1;
       }
-      try (PrintWriter out = resp.getWriter()) {
-         out.write(result);
+      ArrayList<SellerEntity> listSeller = SellerDAO.getInstance().gets((page - 1) * 10, 10);
+      List<String> listJsonStr = new ArrayList<>();
+
+      for (SellerEntity seller : listSeller) {
+         listJsonStr.add(seller.toJson());
       }
+      ServletUtils.printWrite(resp, "[" + String.join(", ", listJsonStr) + "]");
    }
 
    @Override
@@ -73,87 +97,83 @@ public class SellerServlet extends HttpServlet {
       resp.setContentType("text/html; charset=UTF-8");
       String result = "";
 
-      if (isAuthentication(req)) {
-         try {
-            String lastName = req.getParameter("last-name");
-            String firstName = req.getParameter("first-name");
+      try {
+         String lastName = req.getParameter("last-name");
+         String firstName = req.getParameter("first-name");
 
-            String gender = req.getParameter("gender");
-            String dateOfBirth = req.getParameter("dob");
-            String phoneNumber = req.getParameter("phone-number");
-            String email = req.getParameter("email");
-            String ssn = req.getParameter("ssn");
+         String gender = req.getParameter("gender");
+         String dateOfBirth = req.getParameter("dob");
+         String phoneNumber = req.getParameter("phone-number");
+         String email = req.getParameter("email");
+         String ssn = req.getParameter("ssn");
 
-            String provinceId = req.getParameter("province");
-            String districtId = req.getParameter("district");
-            String communeId = req.getParameter("commune");
-            String addressStreet = req.getParameter("address-street");
+         String provinceId = req.getParameter("province");
+         String districtId = req.getParameter("district");
+         String communeId = req.getParameter("commune");
+         String addressStreet = req.getParameter("address-street");
 
-            String storeName = req.getParameter("store-name");
-            String storeLink = req.getParameter("store-link");
-            String businessLicenseld = req.getParameter("business-license-id");
-            String bankAccountId = req.getParameter("bank-account-id");
+         String storeName = req.getParameter("store-name");
+         String storeLink = req.getParameter("store-link");
+         String businessLicenseld = req.getParameter("business-license-id");
+         String bankAccountId = req.getParameter("bank-account-id");
 
-            String image = req.getParameter("image");
+         String image = req.getParameter("image");
 
 
-            UserEntity newUser = new UserEntity(
-                    lastName,
-                    firstName,
-                    gender,
-                    DateTimeUtils.stringToDate(dateOfBirth, "yyyy-MM-dd"),
-                    ssn,
-                    phoneNumber,
-                    email,
-                    phoneNumber, // default username
-                    HashUtils.getMd5(Base64Utils.encodeFromString(phoneNumber)),
-                    Cons.User.USER_TYPE_SELLER
+         UserEntity newUser = new UserEntity(
+                 lastName,
+                 firstName,
+                 gender,
+                 DateTimeUtils.stringToDate(dateOfBirth, "yyyy-MM-dd"),
+                 ssn,
+                 phoneNumber,
+                 email,
+                 phoneNumber, // default username
+                 HashUtils.getMd5(Base64Utils.encodeFromString(phoneNumber)),
+                 Cons.User.USER_TYPE_SELLER
+         );
+
+         if (image != null && !image.isEmpty()) {
+            newUser.setImage(image);
+         }
+
+         SellerEntity newSeller = new SellerEntity();
+         newSeller.setUserEntity(newUser);
+         newSeller.setStoreName(storeName);
+         newSeller.setStoreLink(storeLink);
+         newSeller.setBusinessLicenseId(businessLicenseld);
+         newSeller.setBankAccountId(StringUtils.toLong(bankAccountId));
+
+         Long newSellerId = SellerDAO.getInstance().insert(newSeller);
+
+         if (newSellerId != 0) {
+            AddressEntity newAddress = new AddressEntity(
+                    addressStreet,
+                    communeId,
+                    districtId,
+                    provinceId,
+                    newSellerId,
+                    lastName + " " + firstName,
+                    Cons.Address.DEFAULT_ADDRESS_NAME,
+                    phoneNumber
             );
 
-            if (image != null && !image.isEmpty()) {
-               newUser.setImage(image);
-            }
+            Long newAddressId = AddressDAO.getInstance().insert(newAddress);
 
-            SellerEntity newSeller = new SellerEntity();
-            newSeller.setUserEntity(newUser);
-            newSeller.setStoreName(storeName);
-            newSeller.setStoreLink(storeLink);
-            newSeller.setBusinessLicenseId(businessLicenseld);
-            newSeller.setBankAccountId(StringUtils.toLong(bankAccountId));
+            if (newAddressId != 0) {
+               UserEntity user = UserDAO.getInstance().getById(newSellerId);
+               user.setAddressEntities(Collections.singletonList(AddressDAO.getInstance().getById(newAddressId)));
+               UserDAO.getInstance().update(user);
 
-            Long newSellerId = SellerDAO.getInstance().insert(newSeller);
-
-            if (newSellerId != 0) {
-               AddressEntity newAddress = new AddressEntity(
-                       addressStreet,
-                       communeId,
-                       districtId,
-                       provinceId,
-                       newSellerId,
-                       lastName + " " + firstName,
-                       Cons.Address.DEFAULT_ADDRESS_NAME,
-                       phoneNumber
-               );
-
-               Long newAddressId = AddressDAO.getInstance().insert(newAddress);
-
-               if (newAddressId != 0) {
-                  UserEntity user = UserDAO.getInstance().getById(newSellerId);
-                  user.setAddressEntities(Collections.singletonList(AddressDAO.getInstance().getById(newAddressId)));
-                  UserDAO.getInstance().update(user);
-
-                  result += "true\n" + newSellerId.toString();
-               } else {
-                  result += "false\nError while insert address";
-               }
+               result += "true\n" + newSellerId.toString();
             } else {
-               result += "false\nError while insert user";
+               result += "false\nError while insert address";
             }
-         } catch (Exception e) {
-            result += "false\n" + e.getMessage();
+         } else {
+            result += "false\nError while insert user";
          }
-      } else {
-         result = "Access denied";
+      } catch (Exception e) {
+         result += "false\n" + e.getMessage();
       }
       try (PrintWriter out = resp.getWriter()) {
          out.write(result);
@@ -164,83 +184,79 @@ public class SellerServlet extends HttpServlet {
    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
       resp.setContentType("text/html; charset=UTF-8");
       String result = "";
-      Map<String,Object> parameterMap = ServletUtils.getParametersMap(req);
+      Map<String, Object> parameterMap = ServletUtils.getParametersMap(req);
 
-      if (isAuthentication(req)) {
-         try {
-            String id = (String) parameterMap.get("id");
-            String lastName = (String) parameterMap.get("last-name");
-            String firstName = (String) parameterMap.get("first-name");
+      try {
+         String id = (String) parameterMap.get("id");
+         String lastName = (String) parameterMap.get("last-name");
+         String firstName = (String) parameterMap.get("first-name");
 
-            String gender = (String) parameterMap.get("gender");
-            String dateOfBirth = (String) parameterMap.get("dob");
-            String phoneNumber = (String) parameterMap.get("phone-number");
-            String ssn = (String) parameterMap.get("ssn");
-            String email = (String) parameterMap.get("email");
+         String gender = (String) parameterMap.get("gender");
+         String dateOfBirth = (String) parameterMap.get("dob");
+         String phoneNumber = (String) parameterMap.get("phone-number");
+         String ssn = (String) parameterMap.get("ssn");
+         String email = (String) parameterMap.get("email");
 
-            String provinceId = (String) parameterMap.get("province");
-            String districtId = (String) parameterMap.get("district");
-            String communeId = (String) parameterMap.get("commune");
-            String addressStreet = (String) parameterMap.get("address-street");
+         String provinceId = (String) parameterMap.get("province");
+         String districtId = (String) parameterMap.get("district");
+         String communeId = (String) parameterMap.get("commune");
+         String addressStreet = (String) parameterMap.get("address-street");
 
-            String storeName = (String) parameterMap.get("store-name");
-            String storeLink = (String) parameterMap.get("store-link");
-            String businessLicenseId = (String) parameterMap.get("business-license-id");
-            String bankAccountId = (String) parameterMap.get("bank-account-id");
+         String storeName = (String) parameterMap.get("store-name");
+         String storeLink = (String) parameterMap.get("store-link");
+         String businessLicenseId = (String) parameterMap.get("business-license-id");
+         String bankAccountId = (String) parameterMap.get("bank-account-id");
 
-            UserEntity updateUser = UserDAO.getInstance().getById(StringUtils.toLong(id));
+         UserEntity updateUser = UserDAO.getInstance().getById(StringUtils.toLong(id));
 
-            updateUser.setFirstName(firstName);
-            updateUser.setLastName(lastName);
-            updateUser.setGender(gender);
-            updateUser.setDateOfBirth(DateTimeUtils.stringToDate(dateOfBirth, "dd/MM/yyyy"));
-            updateUser.setPhoneNumber(phoneNumber);
-            updateUser.setSsn(ssn);
-            updateUser.setEmail(email);
+         updateUser.setFirstName(firstName);
+         updateUser.setLastName(lastName);
+         updateUser.setGender(gender);
+         updateUser.setDateOfBirth(DateTimeUtils.stringToDate(dateOfBirth, "dd/MM/yyyy"));
+         updateUser.setPhoneNumber(phoneNumber);
+         updateUser.setSsn(ssn);
+         updateUser.setEmail(email);
 
-            updateUser.setUserType(Cons.User.USER_TYPE_SELLER);
-            updateUser.setUserName(phoneNumber);
-            updateUser.setPassword(HashUtils.getMd5(Base64Utils.encodeFromString(phoneNumber)));
-            updateUser.setStatus(true);
+         updateUser.setUserType(Cons.User.USER_TYPE_SELLER);
+         updateUser.setUserName(phoneNumber);
+         updateUser.setPassword(HashUtils.getMd5(Base64Utils.encodeFromString(phoneNumber)));
+         updateUser.setStatus(true);
 
-            Boolean updateResult = UserDAO.getInstance().update(updateUser);
+         Boolean updateResult = UserDAO.getInstance().update(updateUser);
 
-            if (updateResult != false) {
-               AddressEntity updateAddress = updateUser.getAddressEntities().get(0);
+         if (updateResult != false) {
+            AddressEntity updateAddress = updateUser.getAddressEntities().get(0);
 
-               updateAddress.setStreet(addressStreet);
-               updateAddress.setProvinceEntity(ProvinceDAO.getInstance().getById(provinceId));
-               updateAddress.setDistrictEntity(DistrictDAO.getInstance().getById(districtId));
-               updateAddress.setCommuneEntity(CommuneDAO.getInstance().getById(communeId));
+            updateAddress.setStreet(addressStreet);
+            updateAddress.setProvinceEntity(ProvinceDAO.getInstance().getById(provinceId));
+            updateAddress.setDistrictEntity(DistrictDAO.getInstance().getById(districtId));
+            updateAddress.setCommuneEntity(CommuneDAO.getInstance().getById(communeId));
 
-               updateAddress.setFullName(lastName + " " + firstName);
-               updateAddress.setAddressName(Cons.Address.DEFAULT_ADDRESS_NAME);
-               updateAddress.setUserId(updateUser.getUserId());
+            updateAddress.setFullName(lastName + " " + firstName);
+            updateAddress.setAddressName(Cons.Address.DEFAULT_ADDRESS_NAME);
+            updateAddress.setUserId(updateUser.getUserId());
 
-               Boolean updateAddressResult = AddressDAO.getInstance().update(updateAddress);
+            Boolean updateAddressResult = AddressDAO.getInstance().update(updateAddress);
 
-               if (updateAddressResult != false) {
+            if (updateAddressResult != false) {
 
-                  SellerEntity sellerEntity = SellerDAO.getInstance().getById(updateUser.getUserId());
-                  sellerEntity.setStoreName(storeName);
-                  sellerEntity.setStoreLink(storeLink);
-                  sellerEntity.setBusinessLicenseId(businessLicenseId);
-                  sellerEntity.setBankAccountId(StringUtils.toLong(bankAccountId));
+               SellerEntity sellerEntity = SellerDAO.getInstance().getById(updateUser.getUserId());
+               sellerEntity.setStoreName(storeName);
+               sellerEntity.setStoreLink(storeLink);
+               sellerEntity.setBusinessLicenseId(businessLicenseId);
+               sellerEntity.setBankAccountId(StringUtils.toLong(bankAccountId));
 
-                  SellerDAO.getInstance().update(sellerEntity);
+               SellerDAO.getInstance().update(sellerEntity);
 
-                  result += "true\n" + updateUser.getUserId().toString();
-               } else {
-                  result += "false\nError while update seller";
-               }
+               result += "true\n" + updateUser.getUserId().toString();
             } else {
                result += "false\nError while update seller";
             }
-         } catch (Exception e) {
-            result += "false\n" + e.getMessage();
+         } else {
+            result += "false\nError while update seller";
          }
-      } else {
-         result = "Access denied";
+      } catch (Exception e) {
+         result += "false\n" + e.getMessage();
       }
       try (PrintWriter out = resp.getWriter()) {
          out.write(result);
