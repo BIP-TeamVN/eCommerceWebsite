b8442e4d1f20ea586624f38c9f985d122cfe643a
QuocNam <namtranquoc322@gmail.com>
2021-01-18 14:59:49 +0700

    api list product by seller id
    status = 1

--- src/main/java/com/hknp/model/dao/ProductDAO.java
+++ src/main/java/com/hknp/model/dao/ProductDAO.java
@@ -333,4 +333,36 @@ public class ProductDAO implements IRetrieveEntity<ProductEntity, Long>, IModify
       Long result = (Long) query.getSingleResult();
       return result == null ? 0 : result;
    }
+
+   public ArrayList<ProductEntity> getProductBySellerId(Integer firstResult, Integer maxResults, Long sellerId) {
+      EntityManager entityMgr = EntityUtils.getEntityManager();
+
+      Query query;
+      String queryStr;
+
+      queryStr = "SELECT u FROM ProductEntity u " +
+              "where u.sellerEntity.userId = :sellerIdPara " +
+              "and u.status = 1";
+
+      query = entityMgr.createQuery(queryStr, ProductEntity.class);
+
+      query.setParameter("sellerIdPara", sellerId);
+
+      if (firstResult != null) {
+         query.setFirstResult(firstResult);
+      }
+      if (maxResults != null) {
+         query.setMaxResults(maxResults);
+      }
+
+      ArrayList<ProductEntity> result = null;
+      try {
+         result = new ArrayList<>(query.getResultList());
+      } catch (Exception exception) {
+         exception.printStackTrace();
+      } finally {
+         entityMgr.close();
+      }
+      return result;
+   }
 }
