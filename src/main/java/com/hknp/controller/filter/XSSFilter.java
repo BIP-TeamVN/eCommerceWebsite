193604dab6a3344ed2ae697822a55cca5bcd112d
hohuyhoangg <hoangho1147@gmail.com>
2021-06-20 17:05:07 +0700

    add

--- src/main/java/com/hknp/controller/filter/XSSFilter.java
+++ src/main/java/com/hknp/controller/filter/XSSFilter.java
@@ -3,6 +3,7 @@ package com.hknp.controller.filter;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = {"/*"})
@@ -13,6 +14,17 @@ public class XSSFilter implements Filter {
    @Override
    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
       XSSRequestWrapper filterRes = new XSSRequestWrapper((HttpServletRequest) req);
+      HttpServletResponse res = (HttpServletResponse) resp;
+
+      res.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
+      res.setHeader("Pragma","no-cache");
+      res.setDateHeader("Expires",0);
+
+      res.setHeader("X-XSS-Protection","1; mode=block");
+
+      res.setHeader("X-Content-Type-Options","nosniff");
+
+      res.setHeader("X-Frame-Options","DENY");
 
       chain.doFilter(filterRes, resp);
    }
